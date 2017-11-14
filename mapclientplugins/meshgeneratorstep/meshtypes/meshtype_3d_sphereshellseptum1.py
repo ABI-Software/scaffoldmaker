9e296c3469a435916daa567285659861984388d6
Richard Christie <r.christie@auckland.ac.nz>
2017-11-15 11:08:22 +1300

    Share common interpolation functions

--- mapclientplugins/meshgeneratorstep/meshtypes/meshtype_3d_sphereshellseptum1.py
+++ mapclientplugins/meshgeneratorstep/meshtypes/meshtype_3d_sphereshellseptum1.py
@@ -8,40 +8,10 @@ Only one element throught the wall is currently implemented.
 """
 
 import math
+from mapclientplugins.meshgeneratorstep.utils.interpolation import interpolateCubicHermite, interpolateCubicHermiteDerivative
 from opencmiss.zinc.element import Element, Elementbasis, Elementfieldtemplate
 from opencmiss.zinc.field import Field
 from opencmiss.zinc.node import Node
-
-def interpolateCubicHermite(v1, d1, v2, d2, xi):
-    """
-    Return cubic Hermite interpolated value of tuples v1, d1 (end 1) to v2, d2 (end 2) for xi in [0,1]
-    :return: tuple containing result
-    """
-    xi2 = xi*xi
-    xi3 = xi2*xi
-    f1 = 1.0 - 3.0*xi2 + 2.0*xi3
-    f2 = xi - 2.0*xi2 + xi3
-    f3 = 3.0*xi2 - 2.0*xi3
-    f4 = -xi2 + xi3
-    result = []
-    for i in range(len(v1)):
-        result.append(f1*v1[i] + f2*d1[i] + f3*v2[i] + f4*d2[i])
-    return tuple(result)
-
-def interpolateCubicHermiteDerivative(v1, d1, v2, d2, xi):
-    """
-    Return cubic Hermite interpolated derivatives of tuples v1, d1 (end 1) to v2, d2 (end 2) for xi in [0,1]
-    :return: tuple containing result
-    """
-    xi2 = xi*xi
-    f1 = -6.0*xi + 6.0*xi2
-    f2 = 1.0 - 4.0*xi + 3.0*xi2
-    f3 = 6.0*xi - 6.0*xi2
-    f4 = -2.0*xi + 3.0*xi2
-    result = []
-    for i in range(len(v1)):
-        result.append(f1*v1[i] + f2*d1[i] + f3*v2[i] + f4*d2[i])
-    return tuple(result)
 
 class MeshType_3d_sphereshellseptum1(object):
     '''
