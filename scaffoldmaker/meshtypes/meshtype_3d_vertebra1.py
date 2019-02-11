09f01d2ced5720e75297ef7bc0274266c8630e50
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2019-02-12 11:38:22 +1300

    Create nodes.

--- scaffoldmaker/meshtypes/meshtype_3d_vertebra1.py
+++ scaffoldmaker/meshtypes/meshtype_3d_vertebra1.py
@@ -27,7 +27,7 @@ class MeshType_3d_vertebra1(Scaffold_base):
 
     @staticmethod
     def getDefaultOptions(parameterSetName='Default'):
-        return {'Number of elements around': 4, 'Number of elements up': 4, 'Number of elements through wall': 1,
+        return {'Number of elements around': 4, 'Number of elements up': 3, 'Number of elements through wall': 2,
             'Number of elements radial': 1, 'Major diameter': 2.0, 'Minor diameter': 1.0, 'Height': 1.0,
             'Use cross derivatives': False, 'Refine': False, 'Refine number of elements around': 1,
             'Refine number of elements up': 1, 'Refine number of elements radial': 1}
@@ -47,10 +47,10 @@ class MeshType_3d_vertebra1(Scaffold_base):
                 options[key] = 1
         if options['Number of elements up'] < 2:
             options['Number of elements up'] = 2
-        if options['Number of elements around'] < 2:
+        if options['Number of elements around'] < 4:
+            options['Number of elements around'] = 4
+        if options['Number of elements through wall'] < 2:
             options['Number of elements around'] = 2
-        if options['Number of elements through wall'] < 1:
-            options['Number of elements around'] = 1
         if options['Major diameter'] < 0.0:
             options['Diameter'] = 0.0
         if options['Minor diameter'] < 0.0:
@@ -81,13 +81,56 @@ class MeshType_3d_vertebra1(Scaffold_base):
         """ Get nodes along the centre line """
         cx = [zero, [0.0, 0.0, height]]
         cd1 = [[0.0, 0.0, height / elementsCountUp], [0.0, 0.0, height / elementsCountUp]]
+        cd2 = [[0.0, axisB / elementsCountThroughWall, 0.0], [0.0, axisB / elementsCountThroughWall, 0.0]]
+        cd3 = [[axisA / elementsCountThroughWall, 0.0, 0.0], [axisA / elementsCountThroughWall, 0.0, 0.0]]
+        t2 = [0.5, 0.5]
+        t2d = [0.0, 0.0]
+        t3 = [0.5, 0.5]
+        t3d = [0.0, 0.0]
+
         sx, sd1, se, sxi, _ = sampleCubicHermiteCurves(cx, cd1, elementsCountUp)
-        # sd2 = interpolateSampleLinear(cd2, se, sxi)
-        # sd3 = interpolateSampleLinear(cd3, se, sxi)
-        # st2 = interpolateSampleLinear(t2, se, sxi)
-        # st2d = interpolateSampleLinear(t2d, se, sxi)
-        # st3 = interpolateSampleLinear(t3, se, sxi)
-        # st3d = interpolateSampleLinear(t3d, se, sxi)
+        sd2 = interpolateSampleLinear(cd2, se, sxi)
+        sd3 = interpolateSampleLinear(cd3, se, sxi)
+        st2 = interpolateSampleLinear(t2, se, sxi)
+        st2d = interpolateSampleLinear(t2d, se, sxi)
+        st3 = interpolateSampleLinear(t3, se, sxi)
+        st3d = interpolateSampleLinear(t3d, se, sxi)
+
+        # Find unit normals and binormals at each sample points
+        sNormal = []
+        sBinormal = []
+
+        # Set up normal and binormal for first frame
+        prevUnitTangent = normalise(sd1[0])
+        if magnitude(crossproduct3(prevUnitTangent, [0.0, 0.0, 1.0])) > 0.0:
+            prevBinormal = crossproduct3(prevUnitTangent, [0.0, 0.0, 1.0])
+        else:
+            prevBinormal = crossproduct3(prevUnitTangent, [0.0, -1.0, 0.0])
+        prevUnitBinormal = normalise(prevBinormal)
+        prevUnitNormal = crossproduct3(prevUnitBinormal, prevUnitTangent)
+        sNormal.append(prevUnitNormal)
+        sBinormal.append(prevUnitBinormal)
+
+        # Step through central line and rotate central line axes to align tangent
+        # to tangent from previous frame
+        for n in range(1, elementsCountUp + 1):
+            unitTangent = normalise(sd1[n])
+            cp = crossproduct3(prevUnitTangent, unitTangent)
+            if magnitude(cp) > 0.0:
+                axisRot = normalise(cp)
+                thetaRot = math.acos(dotproduct(prevUnitTangent, unitTangent))
+                rotFrame = rotationMatrixAboutAxis(axisRot, thetaRot)
+                rotNormal = [rotFrame[j][0] * prevUnitNormal[0] + rotFrame[j][1] * prevUnitNormal[1] + rotFrame[j][2] *
+                             prevUnitNormal[2] for j in range(3)]
+                unitNormal = normalise(rotNormal)
+                unitBinormal = crossproduct3(unitTangent, unitNormal)
+                prevUnitTangent = unitTangent
+                prevUnitNormal = unitNormal
+            else:
+                unitBinormal = prevUnitBinormal
+                unitNormal = prevUnitNormal
+            sNormal.append(unitNormal)
+            sBinormal.append(unitBinormal)
 
         fm = region.getFieldmodule()
         fm.beginChange()
@@ -175,6 +218,115 @@ class MeshType_3d_vertebra1(Scaffold_base):
             coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS2DS3, 1, zero)
             coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
         nodeIdentifier = nodeIdentifier + 1
+
+        # Create nodes on the surface
+        for n3 in range(elementsCountThroughWall):
+            for n2 in range(elementsCountUp + 1):
+                aThroughWallElement = sd2[n2][1] + st2[n2] * (n3 / elementsCountThroughWall)
+                bThroughWallElement = sd3[n2][0] + st3[n2] * (n3 / elementsCountThroughWall)
+                perimeterAroundWallElement = getApproximateEllipsePerimeter(aThroughWallElement, bThroughWallElement)
+                arcLengthPerElementAround = perimeterAroundWallElement / elementsCountAround
+                prevRadiansAround = updateEllipseAngleByArcLength(aThroughWallElement, bThroughWallElement, 0.0,
+                                                                  arcLengthPerElementAround)
+                st2PerWallElement = st2[n2] / elementsCountThroughWall
+                st3PerWallElement = st3[n2] / elementsCountThroughWall
+
+                if n2 < elementsCountUp:
+                    aThroughWallElementNext = sd2[n2 + 1][1] + st2[n2 + 1] * (n3 / elementsCountThroughWall)
+                    bThroughWallElementNext = sd3[n2 + 1][0] + st3[n2 + 1] * (n3 / elementsCountThroughWall)
+                    perimeterAroundWallElementNext = getApproximateEllipsePerimeter(aThroughWallElementNext,
+                                                                                    bThroughWallElementNext)
+                    arcLengthPerElementAroundNext = perimeterAroundWallElementNext / elementsCountAround
+
+                for n1 in range(elementsCountAround):
+                    arcLengthAround = n1 * arcLengthPerElementAround
+                    radiansAround = -1 * updateEllipseAngleByArcLength(aThroughWallElement, bThroughWallElement, 0.0,
+                                                                       arcLengthAround)
+                    cosRadiansAround = math.cos(radiansAround)
+                    sinRadiansAround = math.sin(radiansAround)
+                    x = [sx[n2][j] + aThroughWallElement * cosRadiansAround * sBinormal[n2][
+                        j] + bThroughWallElement * sinRadiansAround * sNormal[n2][j] for j in range(3)]
+                    dx_ds1 = [(radiansAround - prevRadiansAround) * (
+                                aThroughWallElement * -sinRadiansAround * sBinormal[n2][
+                            j] + bThroughWallElement * cosRadiansAround * sNormal[n2][j]) for j in range(3)]
+
+                    # Calculate curvature to find d1 for node
+                    unitNormal = normalise([aThroughWallElement * cosRadiansAround * sBinormal[n2][
+                        j] + bThroughWallElement * sinRadiansAround * sNormal[n2][j] for j in range(3)])
+                    if n2 == 0:
+                        curvature = getCubicHermiteCurvature(sx[n2], sd1[n2], sx[n2 + 1], sd1[n2 + 1], unitNormal, 0.0)
+                    elif n2 == elementsCountUp:
+                        curvature = getCubicHermiteCurvature(sx[n2 - 1], sd1[n2 - 1], sx[n2], sd1[n2], unitNormal, 1.0)
+                    else:
+                        curvature = 0.5 * (
+                                getCubicHermiteCurvature(sx[n2 - 1], sd1[n2 - 1], sx[n2], sd1[n2], unitNormal,
+                                                         1.0) + getCubicHermiteCurvature(sx[n2], sd1[n2], sx[n2 + 1],
+                                                                                         sd1[n2 + 1], unitNormal, 0.0))
+                    wallDistance = magnitude([aThroughWallElement * cosRadiansAround * sBinormal[n2][
+                        j] + bThroughWallElement * sinRadiansAround * sNormal[n2][j] for j in range(3)])
+                    factor = 1.0 - curvature * wallDistance
+                    d1Wall = [factor * c for c in sd1[n2]]
+
+                    # Calculate curvature to find d1 for downstream node
+                    if n2 < elementsCountUp:
+                        arcLengthAroundNext = n1 * arcLengthPerElementAroundNext
+                        radiansAroundNext = -1 * updateEllipseAngleByArcLength(aThroughWallElementNext,
+                                                                               bThroughWallElementNext, 0.0,
+                                                                               arcLengthAroundNext)
+                        cosRadiansAroundNext = math.cos(radiansAroundNext)
+                        sinRadiansAroundNext = math.sin(radiansAroundNext)
+                        xNext = [sx[n2 + 1][j] + aThroughWallElementNext * cosRadiansAroundNext * sBinormal[n2 + 1][
+                            j] + bThroughWallElementNext * sinRadiansAroundNext * sNormal[n2 + 1][j] for j in range(3)]
+                        unitNormalNext = normalise([aThroughWallElementNext * cosRadiansAroundNext * sBinormal[n2 + 1][
+                            j] + bThroughWallElementNext * sinRadiansAroundNext * sNormal[n2 + 1][j] for j in range(3)])
+                        if n2 + 1 == elementsCountUp:
+                            curvatureNext = getCubicHermiteCurvature(sx[n2], sd1[n2], sx[n2 + 1], sd1[n2 + 1],
+                                                                     unitNormalNext, 1.0)
+                        else:
+                            curvatureNext = 0.5 * (
+                                    getCubicHermiteCurvature(sx[n2], sd1[n2], sx[n2 + 1], sd1[n2 + 1], unitNormalNext,
+                                                             1.0) + getCubicHermiteCurvature(sx[n2 + 1], sd1[n2 + 1],
+                                                                                             sx[n2 + 2], sd1[n2 + 2],
+                                                                                             unitNormalNext, 0.0))
+                        wallDistanceNext = magnitude([aThroughWallElementNext * cosRadiansAroundNext *
+                                                      sBinormal[n2 + 1][
+                                                          j] + bThroughWallElementNext * sinRadiansAroundNext *
+                                                      sNormal[n2 + 1][j] for j in range(3)])
+                        factorNext = 1.0 - curvatureNext * wallDistanceNext
+                        d1WallNext = [factorNext * c for c in sd1[n2 + 1]]
+                        arcLength = computeCubicHermiteArcLength(x, d1Wall, xNext, d1WallNext, True)
+                        dx_ds2 = [arcLength * c for c in normalise(d1Wall)]
+                        if n2 == elementsCountUp - 1:
+                            secondLastX = x
+                            secondLastd1Wall = d1Wall
+                            lastX = xNext
+                            lastd1Wall = d1WallNext
+                    else:
+                        arcLength = computeCubicHermiteArcLength(secondLastX, secondLastd1Wall, lastX, lastd1Wall, True)
+                        dx_ds2 = [arcLength * c for c in normalise(lastd1Wall)]
+
+                    dx_ds3 = [
+                        st2PerWallElement * cosRadiansAround * sBinormal[n2][j] + st3PerWallElement * sinRadiansAround *
+                        sNormal[n2][j] for j in range(3)]  # Modify later to calculate with interpolation
+
+                    node = nodes.createNode(nodeIdentifier, nodetemplate)
+                    cache.setNode(node)
+                    coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, x)
+                    coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS1, 1, dx_ds1)
+                    coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS2, 1, dx_ds2)
+                    if useCubicHermiteThroughWall:
+                        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS3, 1, dx_ds3)
+                    if useCrossDerivatives:
+                        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                        if useCubicHermiteThroughWall:
+                            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+                    nodeIdentifier = nodeIdentifier + 1
+                    prevRadiansAround = radiansAround
+
+
+        fm.endChange()
 
     @classmethod
     def generateMesh(cls, region, options):
