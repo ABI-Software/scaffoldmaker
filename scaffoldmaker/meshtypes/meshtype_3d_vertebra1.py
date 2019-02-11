d1f3a821798c2804fa582c5b0a2a5d260c53e0c7
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2019-02-11 18:13:04 +1300

    Attempt to create nodes along the centre line.

--- scaffoldmaker/meshtypes/meshtype_3d_vertebra1.py
+++ scaffoldmaker/meshtypes/meshtype_3d_vertebra1.py
@@ -4,20 +4,21 @@ articular processes, vertebral arch and pedicle.
 """
 
 from __future__ import division
+
 import math
-from scaffoldmaker.annotation.annotationgroup import AnnotationGroup, findAnnotationGroupByName
-from scaffoldmaker.meshtypes.meshtype_3d_heartatria1 import MeshType_3d_heartatria1
-from scaffoldmaker.meshtypes.meshtype_3d_heartventriclesbase1 import MeshType_3d_heartventriclesbase1
-from scaffoldmaker.utils.eft_utils import *
-from scaffoldmaker.utils.eftfactory_bicubichermitelinear import eftfactory_bicubichermitelinear
-from scaffoldmaker.utils.zinc_utils import *
+
+from scaffoldmaker.meshtypes.scaffold_base import Scaffold_base
 from scaffoldmaker.utils.meshrefinement import MeshRefinement
-from opencmiss.zinc.element import Element
+from scaffoldmaker.utils.torusmesh import *
+from scaffoldmaker.utils.zinc_utils import *
+from scaffoldmaker.utils.geometry import *
+from scaffoldmaker.utils.interpolation import *
+from scaffoldmaker.utils.vector import *
 
 
-class MeshType_3d_vertebra1(object):
+class MeshType_3d_vertebra1(Scaffold_base):
     """
-    Generates a 3-D vertebral model.
+    Generates a 3-D vertebral mesh
     """
 
     @staticmethod
@@ -25,90 +26,280 @@ class MeshType_3d_vertebra1(object):
         return '3D Vertebra 1'
 
     @staticmethod
-    def getDefaultOptions():
-        return {
-            'Type of vertebra' : "Thoracic",  # Cervical, Thoracic, Lumbar
-            'Number of elements for vertebral body' : 8,
-            'Unit scale': 1.0,
-            'Vertebral body semi-axis a length factor' : 1,
-            'Vertebral body semi-axis b length factor' : 1,
-            'Vertebral body semi-axis c length factor' : 1,
-            'Vertebral body posterior curvature factor' : 1,
-            'Refine': False,
-            'Refine number of elements surface': 4,
-            'Refine number of elements through body': 1,
-            'Use cross derivatives': False,
-        }
+    def getDefaultOptions(parameterSetName='Default'):
+        return {'Number of elements around': 4, 'Number of elements up': 4, 'Number of elements through wall': 1,
+            'Number of elements radial': 1, 'Major diameter': 2.0, 'Minor diameter': 1.0, 'Height': 1.0,
+            'Use cross derivatives': False, 'Refine': False, 'Refine number of elements around': 1,
+            'Refine number of elements up': 1, 'Refine number of elements radial': 1}
 
     @staticmethod
     def getOrderedOptionNames():
-        return [
-            'Type of vertebra',
-            'Number of elements around atrial free wall',
-            'Unit scale',
-            'Vertebral body semi-axis a length factor',
-            'Vertebral body semi-axis b length factor',
-            'Vertebral body semi-axis c length factor',
-            'Vertebral body posterior curvature factor',
-            'Refine',
-            'Refine number of elements surface',
-            'Refine number of elements through body',
-            #,'Use cross derivatives'
-        ]
+        return ['Number of elements around', 'Number of elements radial', 'Number of elements up',
+            'Number of elements through wall', 'Major diameter', 'Minor diameter', 'Height', 'Use cross derivatives',
+            'Refine', 'Refine number of elements around', 'Refine number of elements up',
+            'Refine number of elements radial']
 
     @staticmethod
     def checkOptions(options):
-        """
-        :return:  True if dependent options changed, otherwise False. This
-        happens where two or more options must change together to be valid.
-        """
-        return None
+        for key in ['Number of elements radial', 'Refine number of elements around', 'Refine number of elements up',
+            'Refine number of elements radial']:
+            if options[key] < 1:
+                options[key] = 1
+        if options['Number of elements up'] < 2:
+            options['Number of elements up'] = 2
+        if options['Number of elements around'] < 2:
+            options['Number of elements around'] = 2
+        if options['Number of elements through wall'] < 1:
+            options['Number of elements around'] = 1
+        if options['Major diameter'] < 0.0:
+            options['Diameter'] = 0.0
+        if options['Minor diameter'] < 0.0:
+            options['Diameter'] = 0.0
+        if options['Height'] < 0.0:
+            options['Diameter'] = 0.0
 
     @classmethod
     def generateBaseMesh(cls, region, options):
         """
-        Generate the base tricubic Hermite mesh. See also generateMesh().
+        Generates the base tricubic Hermite mesh for this MeshType. See also generateMesh().
         :param region: Zinc region to define model in. Must be empty.
         :param options: Dict containing options. See getDefaultOptions().
-        :return: list of AnnotationGroup
+        :return: None
         """
-
-        vertebralType = options['Type of vertebra']
-        elementsCountForVertebralBody = options['Number of elements for vertebral body']
-        unitScale = options['Unit scale']
-        bodyAxisALengthFactor = options['Vertebral body semi-axis a length factor']
-        bodyAxisBLengthFactor = options['Vertebral body semi-axis b length factor']
-        bodyAxisCLengthFactor = options['Vertebral body semi-axis c length factor']
-        bodyPosteriorCurvatureFactor = options['Vertebral body posterior curvature factor']
+        elementsCountAround = options['Number of elements around']
+        elementsCountUp = options['Number of elements up']
+        elementsCountRadial = options['Number of elements radial']
+        elementsCountThroughWall = options['Number of elements through wall']
         useCrossDerivatives = options['Use cross derivatives']
+        axisA = options['Major diameter']
+        axisB = options['Minor diameter']
+        height = options['Height']
+        useCubicHermiteThroughWall = True
+
+        zero = [0.0, 0.0, 0.0]
+
+        """ Get nodes along the centre line """
+        cx = [zero, [0.0, 0.0, height]]
+        cd1 = [[0.0, 0.0, height / elementsCountUp], [0.0, 0.0, height / elementsCountUp]]
+        sx, sd1, se, sxi, _ = sampleCubicHermiteCurves(cx, cd1, elementsCountUp)
+        # sd2 = interpolateSampleLinear(cd2, se, sxi)
+        # sd3 = interpolateSampleLinear(cd3, se, sxi)
+        # st2 = interpolateSampleLinear(t2, se, sxi)
+        # st2d = interpolateSampleLinear(t2d, se, sxi)
+        # st3 = interpolateSampleLinear(t3, se, sxi)
+        # st3d = interpolateSampleLinear(t3d, se, sxi)
 
         fm = region.getFieldmodule()
         fm.beginChange()
         coordinates = getOrCreateCoordinateField(fm)
-        cache = fm.createFieldcache()
-
-        ##############
-        # Create nodes
-        ##############
 
         nodes = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        nodetemplateApex = nodes.createNodetemplate()
+        nodetemplateApex.defineField(coordinates)
+        nodetemplateApex.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_VALUE, 1)
+        nodetemplateApex.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS1, 1)
+        nodetemplateApex.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS2, 1)
+        nodetemplateApex.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS3, 1)
+        if useCrossDerivatives:
+            nodetemplate = nodes.createNodetemplate()
+            nodetemplate.defineField(coordinates)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_VALUE, 1)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS1, 1)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS2, 1)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D2_DS1DS2, 1)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS3, 1)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D2_DS1DS3, 1)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D2_DS2DS3, 1)
+            nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D3_DS1DS2DS3, 1)
+        else:
+            nodetemplate = nodetemplateApex
 
-        nodetemplate = nodes.createNodetemplate()
-        nodetemplate.defineField(coordinates)
-        nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_VALUE, 1)
-        nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS1, 1)
-        nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS2, 1)
-        nodetemplate.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS3, 1)
-        # LA/RA inlet elements are linear through the wall, hence their nodes do not have D_DS3 parameters
-        nodetemplateLinearS3 = nodes.createNodetemplate()
-        nodetemplateLinearS3.defineField(coordinates)
-        nodetemplateLinearS3.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_VALUE, 1)
-        nodetemplateLinearS3.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS1, 1)
-        nodetemplateLinearS3.setValueNumberOfVersions(coordinates, -1, Node.VALUE_LABEL_D_DS2, 1)
+        cache = fm.createFieldcache()
 
-        nodeIdentifier = max(1, getMaximumNodeIdentifier(nodes) + 1)
+        mesh = fm.findMeshByDimension(3)
 
+        if useCubicHermiteThroughWall:
+            eftfactory = eftfactory_tricubichermite(mesh, useCrossDerivatives)
+        else:
+            eftfactory = eftfactory_bicubichermitelinear(mesh, useCrossDerivatives)
+        eft = eftfactory.createEftBasic()
 
+        elementtemplate = mesh.createElementtemplate()
+        elementtemplate.setElementShapeType(Element.SHAPE_TYPE_CUBE)
+        result = elementtemplate.defineField(coordinates, -1, eft)
 
+        """ Create nodes """
+        nodeIdentifier = 1
 
+        # Create bottom node
+        node = nodes.createNode(nodeIdentifier, nodetemplate)
+        cache.setNode(node)
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, zero)
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS1, 1, [0.0, 0.0, height / elementsCountUp])
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS2, 1, [0.0, axisB / elementsCountThroughWall, 0.0])
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS3, 1,
+                                      [axisA / elementsCountThroughWall, 0.0, 0.0])
+        if useCrossDerivatives:
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+        nodeIdentifier = nodeIdentifier + 1
 
+        # Create node along the centre line between bottom and top nodes
+        for n2 in range(1, elementsCountUp):
+            node = nodes.createNode(nodeIdentifier, nodetemplate)
+            cache.setNode(node)
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, [0, 0, n2 * height / elementsCountUp])
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS1, 1, [0.0, 0.0, height / elementsCountUp])
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS2, 1, [0.0, axisB / elementsCountThroughWall, 0.0])
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS3, 1,
+                                          [axisA / elementsCountThroughWall, 0.0, 0.0])
+            if useCrossDerivatives:
+                coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+                coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+                coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+                coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+            nodeIdentifier = nodeIdentifier + 1
+
+        # Create top node
+        node = nodes.createNode(nodeIdentifier, nodetemplate)
+        cache.setNode(node)
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_VALUE, 1, [0, 0, height])
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS1, 1, [0.0, 0.0, height/elementsCountUp])
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS2, 1, [0.0, axisB/elementsCountThroughWall, 0.0])
+        coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D_DS3, 1, [axisA/elementsCountThroughWall, 0.0, 0.0])
+        if useCrossDerivatives:
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS2, 1, zero)
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS1DS3, 1, zero)
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D2_DS2DS3, 1, zero)
+            coordinates.setNodeParameters(cache, -1, Node.VALUE_LABEL_D3_DS1DS2DS3, 1, zero)
+        nodeIdentifier = nodeIdentifier + 1
+
+    # @staticmethod
+    # def getDefaultOptions(parameterSetName='Default'):
+    #     return {
+    #         'Number of elements around' : 8,
+    #         'Number of elements along' : 2,
+    #         'Number of elements through wall' : 1,
+    #         'Tube type' : 1,
+    #         'Use cross derivatives' : False,
+    #         'Use linear through wall' : False,
+    #         'Refine' : False,
+    #         'Refine number of elements around' : 1,
+    #         'Refine number of elements along' : 1,
+    #         'Refine number of elements through wall' : 1
+    #     }
+    #
+    # @staticmethod
+    # def getOrderedOptionNames():
+    #     return [
+    #         'Number of elements around',
+    #         'Number of elements along',
+    #         'Number of elements through wall',
+    #         'Tube type',
+    #         'Use cross derivatives',
+    #         'Use linear through wall',
+    #         'Refine',
+    #         'Refine number of elements around',
+    #         'Refine number of elements along',
+    #         'Refine number of elements through wall'
+    #     ]
+    #
+    # @staticmethod
+    # def checkOptions(options):
+    #     for key in [
+    #         'Number of elements along',
+    #         'Number of elements through wall',
+    #         'Refine number of elements around',
+    #         'Refine number of elements along',
+    #         'Refine number of elements through wall']:
+    #         if options[key] < 1:
+    #             options[key] = 1
+    #     if (options['Number of elements around'] < 2) :
+    #         options['Number of elements around'] = 2
+    #     if (options['Tube type'] < 1 or options['Tube type'] > 3 ) :
+    #         options['Tube type'] = 1
+    #
+    # @staticmethod
+    # def generateBaseMesh(region, options):
+    #     """
+    #     Generate the base tricubic Hermite mesh. See also generateMesh().
+    #     :param region: Zinc region to define model in. Must be empty.
+    #     :param options: Dict containing options. See getDefaultOptions().
+    #     :return: None
+    #     """
+    #     elementsCountAround = options['Number of elements around']
+    #     elementsCountAlong = options['Number of elements along']
+    #     elementsCountThroughWall = options['Number of elements through wall']
+    #     tubeType = options['Tube type']
+    #     useCrossDerivatives = options['Use cross derivatives']
+    #     useCubicHermiteThroughWall = not(options['Use linear through wall'])
+    #
+    #     if tubeType == 1:
+    #         # Straight tube
+    #         # cx = [[0.5, 1.5, 0.0], [ 1.0, 0.0, 2.0 ] ]
+    #         # cd1 = [[ 1.0, -3.0, 4.0 ], [ 1.0, -3.0, 4.0 ]]
+    #         # cd2 = [ [ 0.0, 0.3, 0.0 ], [ 0.0, 0.3, 0.0 ]]
+    #         # cd3 = [ [ 0.0, 0.0, 0.5 ], [ 0.0, 0.0, 0.5 ]]
+    #         cx = [[0.5, 1.5, 0.0], [0.75, 0.75, 1.0] ]
+    #         cd1 = [[ 1.0, -3.0, 4.0 ], [1.0, -3.0, 4.0]]
+    #         cd2 = [ [ 0.0, 0.3, 0.0 ], [ 0.0, 0.3, 0.0 ]]
+    #         cd3 = [ [ 0.0, 0.0, 0.5 ], [ 0.0, 0.0, 0.5 ]]
+    #
+    #         # thickness in cd2 and cd3 directions and derivatives (rate of change)
+    #         t2 = [ 0.5, 0.5 ]
+    #         t2d = [ 0.0, 0.0 ]
+    #         t3 = [ 0.5, 0.5 ]
+    #         t3d = [ 0.0, 0.0 ]
+    #
+    #     elif tubeType == 2:
+    #         # Curved tube 1
+    #         cx = [ [ 0.0, 0.0, 0.0], [1.0, 1.0, 0.0], [ 2.0, 0.0, 0.0 ] ]
+    #         cd1 = [ [ 1.0, 1.0, 0.0 ], [ 1.0, 0.0, 0.0 ], [1.0, -1.0, 0.0] ]
+    #         cd2 = [ [ 0.0, 0.1, 0.0 ], [ 0.0, 0.1, 0.0 ], [ 0.0, 0.1, 0.0 ] ]
+    #         cd3 = [ [ 0.0, 0.0, 0.2 ], [ 0.0, 0.0, 0.2 ], [ 0.0, 0.0, 0.2] ]
+    #
+    #         # thickness in cd2 and cd3 directions and derivatives (rate of change)
+    #         t2 = [ 0.1, 0.1, 0.1 ]
+    #         t2d = [ 0.0, 0.0, 0.0 ]
+    #         t3 = [ 0.1, 0.1, 0.1 ]
+    #         t3d = [ 0.0, 0.0, 0.0 ]
+    #
+    #     elif tubeType == 3:
+    #         # Curved tube 2
+    #         cx = [ [ 0.0, 0.0, 1.0], [1.0, 1.0, 2.0], [ 2.0, 0.0, 4.0 ] ]
+    #         cd1 = [ [ 1.0, 1.0, 1.0 ], [ 1.0, 0.0, 2.0 ], [1.0, -1.0, 2.0] ]
+    #         cd2 = [ [ 0.0, 0.2, 0.0 ], [ 0.0, 0.2, 0.0 ], [ 0.0, 0.2, 0.0 ] ]
+    #         cd3 = [ [ 0.0, 0.0, 0.2 ], [ 0.0, 0.0, 0.2 ], [ 0.0, 0.0, 0.2] ]
+    #
+    #         # thickness in cd2 and cd3 directions and derivatives (rate of change)
+    #         t2 = [ 0.1, 0.1, 0.1 ]
+    #         t2d = [ 0.0, 0.0, 0.0 ]
+    #         t3 = [ 0.1, 0.1, 0.1 ]
+    #         t3d = [ 0.0, 0.0, 0.0 ]
+    #
+    #     nextNodeIdentifier, nextElementIdentifier = generatetorusmesh(region, elementsCountAlong, elementsCountAround, elementsCountThroughWall,
+    #         cx, cd1, cd2, cd3, t2, t2d, t3, t3d, useCrossDerivatives, useCubicHermiteThroughWall)
+
+    @classmethod
+    def generateMesh(cls, region, options):
+        """
+        Generate base or refined mesh.
+        :param region: Zinc region to create mesh in. Must be empty.
+        :param options: Dict containing options. See getDefaultOptions().
+        """
+        if not options['Refine']:
+            cls.generateBaseMesh(region, options)
+            return
+
+        refineElementsCountAround = options['Refine number of elements around']
+        refineElementsCountAlong = options['Refine number of elements along']
+        refineElementsCountThroughWall = options['Refine number of elements through wall']
+
+        baseRegion = region.createRegion()
+        cls.generateBaseMesh(baseRegion, options)
+
+        meshrefinement = MeshRefinement(baseRegion, region)
+        meshrefinement.refineAllElementsCubeStandard3d(refineElementsCountAround, refineElementsCountAlong,
+                                                       refineElementsCountThroughWall)
