b943d06a96e9ec3c9e6cd9cb684e287b4c7be367
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2019-02-14 11:20:13 +1300

    Methods to return identity matrix as well as computing outer product of two vectors.

--- scaffoldmaker/utils/vector.py
+++ scaffoldmaker/utils/vector.py
@@ -54,3 +54,19 @@ def diagonal(m):
         yield list(diag(sx, 0))
     for sy in range(1, width):
         yield list(diag(0, sy))
+
+def identity(n):
+    '''
+    :param n: Number of rows (and columns) in n x n output.
+    :return: The identity matrix
+    '''
+    return [[0] * i + [1] + [0] * (n - i - 1) for i in range(n)]
+
+def outerProduct(a, b):
+    '''
+
+    :param a: First input vector
+    :param b: Second input vector
+    :return: The outer product of a and b.
+    '''
+    return b[:, None] * a
