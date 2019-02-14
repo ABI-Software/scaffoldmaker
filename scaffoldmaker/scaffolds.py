99f75cc20d0e3ffac9f75d539175440255d1bcb4
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2019-02-14 14:03:31 +1300

    Adding the ellipiccylinder scaffold.

--- scaffoldmaker/scaffolds.py
+++ scaffoldmaker/scaffolds.py
@@ -26,6 +26,7 @@ from scaffoldmaker.meshtypes.meshtype_3d_stomachhuman1 import MeshType_3d_stomac
 from scaffoldmaker.meshtypes.meshtype_3d_tube1 import MeshType_3d_tube1
 from scaffoldmaker.meshtypes.meshtype_3d_tubeseptum1 import MeshType_3d_tubeseptum1
 from scaffoldmaker.meshtypes.meshtype_3d_vertebra1 import MeshType_3d_vertebra1
+from scaffoldmaker.meshtypes.meshtype_3d_ellipticcylinder1 import MeshType_3d_ellipticcylinder1
 
 
 class Scaffolds(object):
@@ -55,7 +56,8 @@ class Scaffolds(object):
             MeshType_3d_stomachhuman1,
             MeshType_3d_tube1,
             MeshType_3d_tubeseptum1,
-            MeshType_3d_vertebra1
+            MeshType_3d_vertebra1,
+            MeshType_3d_ellipticcylinder1
             ]
 
     def getMeshTypes(self):
