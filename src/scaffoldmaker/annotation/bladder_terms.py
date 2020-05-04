61fae4f9ddd13939a57808bc6609b53eb0fd139b
Zohreh Ekhlasi <zekh167@aucklanduni.ac.nz>
2020-05-04 15:32:12 +1200

    removed the note on bladder body.

--- src/scaffoldmaker/annotation/bladder_terms.py
+++ src/scaffoldmaker/annotation/bladder_terms.py
@@ -4,11 +4,11 @@ Common resource for bladder annotation terms.
 
 # convention: preferred name, preferred id, followed by any other ids and alternative names
 bladder_terms = [
-    ( "urinary bladder", "FMA:15900", "UBERON:0001255" ),
-    ( "neck of urinary bladder", "FMA:15912", "UBERON:0001258"),
-    ( "Dome of the Bladder", None),  # needs to be replaced with an actual term e.g. urinary bladder (which includes neck)
-    ( "Serosa of body of urinary bladder", None),
-    ( "Lumen of body of urinary bladder", None),
+    ("urinary bladder", "FMA:15900", "UBERON:0001255"),
+    ("neck of urinary bladder", "FMA:15912", "UBERON:0001258"),
+    ("Dome of the Bladder", None),
+    ("Serosa of body of urinary bladder", None),
+    ("Lumen of body of urinary bladder", None),
     ("Serosa of neck of urinary bladder", None),
     ("Lumen of neck of urinary bladder", None)
    ]
