c412f9848e78aa865851ffe1db1e700146fbb724
Richard Christie <r.christie@auckland.ac.nz>
2020-06-18 14:44:22 +1200

    Add 2d tube bifurcation scaffold and utils

--- src/scaffoldmaker/scaffolds.py
+++ src/scaffoldmaker/scaffolds.py
@@ -8,6 +8,7 @@ from scaffoldmaker.meshtypes.meshtype_2d_plate1 import MeshType_2d_plate1
 from scaffoldmaker.meshtypes.meshtype_2d_platehole1 import MeshType_2d_platehole1
 from scaffoldmaker.meshtypes.meshtype_2d_sphere1 import MeshType_2d_sphere1
 from scaffoldmaker.meshtypes.meshtype_2d_tube1 import MeshType_2d_tube1
+from scaffoldmaker.meshtypes.meshtype_2d_tubebifurcation1 import MeshType_2d_tubebifurcation1
 from scaffoldmaker.meshtypes.meshtype_3d_bladder1 import MeshType_3d_bladder1
 from scaffoldmaker.meshtypes.meshtype_3d_box1 import MeshType_3d_box1
 from scaffoldmaker.meshtypes.meshtype_3d_boxhole1 import MeshType_3d_boxhole1
@@ -44,6 +45,7 @@ class Scaffolds(object):
             MeshType_2d_platehole1,
             MeshType_2d_sphere1,
             MeshType_2d_tube1,
+            MeshType_2d_tubebifurcation1,
             MeshType_3d_bladder1,
             MeshType_3d_box1,
             MeshType_3d_boxhole1,
