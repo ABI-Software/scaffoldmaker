e70aa685bc9551ef0b1395424cd02c812f93be35
Richard Christie <r.christie@auckland.ac.nz>
2020-08-11 21:00:17 +1200

    Add user-defined annotation groups

--- tests/test_general.py
+++ tests/test_general.py
@@ -5,9 +5,17 @@ from opencmiss.zinc.context import Context
 from opencmiss.zinc.field import Field
 from opencmiss.zinc.node import Node
 from opencmiss.zinc.result import RESULT_OK
+from scaffoldmaker.annotation.annotationgroup import AnnotationGroup
 from scaffoldmaker.meshtypes.meshtype_3d_box1 import MeshType_3d_box1
+from scaffoldmaker.meshtypes.meshtype_3d_heartatria1 import MeshType_3d_heartatria1
 from scaffoldmaker.scaffoldpackage import ScaffoldPackage
+from scaffoldmaker.scaffolds import Scaffolds
 from testutils import assertAlmostEqualList
+
+from scaffoldmaker.utils.zinc_utils import identifier_ranges_from_string, identifier_ranges_to_string, \
+    mesh_group_add_identifier_ranges, mesh_group_to_identifier_ranges, \
+    nodeset_group_add_identifier_ranges, nodeset_group_to_identifier_ranges
+
 
 class GeneralScaffoldTestCase(unittest.TestCase):
 
@@ -76,6 +84,107 @@ class GeneralScaffoldTestCase(unittest.TestCase):
         self.assertEqual(RESULT_OK, result)
         assertAlmostEqualList(self, d3, [  2.499999998128999e-01, -4.330127019169794e-01,  0.000000000000000e+00 ], delta=TOL)
         self.assertAlmostEqual(newScale[2], magnitude(d3), delta=TOL)
+
+    def test_user_annotation_groups(self):
+        """
+        Test user annotation group on heartatria1 scaffold with scaffold package.
+        """
+        scaffoldPackage = ScaffoldPackage(MeshType_3d_heartatria1)
+        # can't add user annotation groups until generate is called()
+        try:
+            annotationGroup = scaffoldPackage.createUserAnnotationGroup()
+            self.assertTrue(False)  # should never get here as above raises expection
+        except:
+            pass
+
+        context = Context("Test")
+        region = context.getDefaultRegion()
+        self.assertTrue(region.isValid())
+        fieldmodule = region.getFieldmodule()
+
+        scaffoldPackage.generate(region)
+
+        annotationGroups = scaffoldPackage.getAnnotationGroups()
+        self.assertEqual(19, len(annotationGroups))
+
+        endocardium_of_la = scaffoldPackage.findAnnotationGroupByName('endocardium of left atrium')
+        self.assertTrue(isinstance(endocardium_of_la, AnnotationGroup))
+        self.assertFalse(scaffoldPackage.isUserAnnotationGroup(endocardium_of_la))
+        self.assertFalse(scaffoldPackage.deleteAnnotationGroup(endocardium_of_la))  # can't delete auto annotation groups
+
+        annotationGroup1 = scaffoldPackage.createUserAnnotationGroup()
+        self.assertEqual('group1', annotationGroup1.getName())  # default name
+        self.assertIsNone(annotationGroup1.getId())
+        self.assertTrue(scaffoldPackage.isUserAnnotationGroup(annotationGroup1))
+        self.assertEqual(-1, annotationGroup1.getDimension())  # -1 = empty
+        group = annotationGroup1.getGroup()
+        self.assertTrue(group.isValid())
+        mesh2d = fieldmodule.findMeshByDimension(2)
+        meshGroup = group.createFieldElementGroup(mesh2d).getMeshGroup()
+        mesh_group_add_identifier_ranges(meshGroup, [[1,2],[4,4]])
+        self.assertEqual(3, meshGroup.getSize())
+        self.assertEqual(2, annotationGroup1.getDimension())
+        identifier_ranges_string = identifier_ranges_to_string(mesh_group_to_identifier_ranges(meshGroup))
+        self.assertEqual('1-2,4', identifier_ranges_string)
+
+        annotationGroup2 = scaffoldPackage.createUserAnnotationGroup(('bob', 'BOB:1'))
+        self.assertEqual('bob', annotationGroup2.getName())
+        self.assertEqual('BOB:1', annotationGroup2.getId())
+        self.assertTrue(scaffoldPackage.isUserAnnotationGroup(annotationGroup2))
+        group = annotationGroup2.getGroup()
+        nodes = fieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        nodesetGroup = group.createFieldNodeGroup(nodes).getNodesetGroup()
+        nodeset_group_add_identifier_ranges(nodesetGroup, identifier_ranges_from_string('1,3-5,7'))
+        self.assertEqual(5, nodesetGroup.getSize())
+        self.assertEqual(0, annotationGroup2.getDimension())
+        identifier_ranges_string = identifier_ranges_to_string(nodeset_group_to_identifier_ranges(nodesetGroup))
+        self.assertEqual('1,3-5,7', identifier_ranges_string)
+
+        annotationGroup3 = scaffoldPackage.createUserAnnotationGroup()
+        self.assertEqual('group2', annotationGroup3.getName())  # default name
+        self.assertTrue(scaffoldPackage.isUserAnnotationGroup(annotationGroup3))
+        annotationGroups = scaffoldPackage.getAnnotationGroups()
+        self.assertEqual(22, len(annotationGroups))
+
+        self.assertTrue(scaffoldPackage.deleteAnnotationGroup(annotationGroup3))
+        annotationGroups = scaffoldPackage.getAnnotationGroups()
+        self.assertEqual(21, len(annotationGroups))
+
+        # test serialisation
+        dct = scaffoldPackage.toDict()
+        self.assertEqual('3D Heart Atria 1', dct['scaffoldTypeName'])
+        scaffoldType = Scaffolds().findScaffoldTypeByName(dct['scaffoldTypeName'])
+
+        scaffoldPackage2 = ScaffoldPackage(scaffoldType, dct)
+        region2 = context.createRegion()
+        fieldmodule2 = region2.getFieldmodule()
+
+        scaffoldPackage2.generate(region2)
+
+        annotationGroups2 = scaffoldPackage2.getAnnotationGroups()
+        self.assertEqual(21, len(annotationGroups2))
+
+        annotationGroup1 = scaffoldPackage2.findAnnotationGroupByName('group1')
+        self.assertEqual('group1', annotationGroup1.getName())
+        self.assertIsNone(annotationGroup1.getId())
+        self.assertTrue(scaffoldPackage2.isUserAnnotationGroup(annotationGroup1))
+        self.assertEqual(2, annotationGroup1.getDimension())
+        mesh2d2 = fieldmodule2.findMeshByDimension(2)
+        meshGroup2 = annotationGroup1.getMeshGroup(mesh2d2)
+        self.assertEqual(3, meshGroup2.getSize())
+        identifier_ranges_string = identifier_ranges_to_string(mesh_group_to_identifier_ranges(meshGroup2))
+        self.assertEqual('1-2,4', identifier_ranges_string)
+
+        annotationGroup2 = scaffoldPackage2.findAnnotationGroupByName('bob')
+        self.assertEqual('bob', annotationGroup2.getName())
+        self.assertEqual('BOB:1', annotationGroup2.getId())
+        self.assertTrue(scaffoldPackage2.isUserAnnotationGroup(annotationGroup2))
+        self.assertEqual(0, annotationGroup2.getDimension())
+        nodes2 = fieldmodule2.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        nodesetGroup2 = annotationGroup2.getNodesetGroup(nodes2)
+        self.assertEqual(5, nodesetGroup.getSize())
+        identifier_ranges_string = identifier_ranges_to_string(nodeset_group_to_identifier_ranges(nodesetGroup2))
+        self.assertEqual('1,3-5,7', identifier_ranges_string)
 
 
 if __name__ == "__main__":
