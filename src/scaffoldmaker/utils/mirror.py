0cbbf8dbfb7c45c4d4dce89c877883f72c2ee1d4
Elias Ghadam Soltani <egha355@UoA.auckland.ac.nz>
2020-08-26 13:42:50 +1200

    make a class from mirror in mirror.py that gets the plane and mirrors the geometry

--- src/scaffoldmaker/utils/mirror.py
+++ src/scaffoldmaker/utils/mirror.py
@@ -5,55 +5,72 @@ Utilities for getting a mirror image of a scaffold.
 from __future__ import division
 from scaffoldmaker.utils import vector
 
-def getPointDistanceFromPlane(x, p):
-    """
-    Get distance from point x to plane p. Returns negative distance, if point x is on the negative side of the plane.
-    :param x: Point coordinates.
-    :param p: Plane ax+by+cd=d defined as a list p=[a,b,c,d].
-    :return: Distance.
-    """
-    n = getPlaneNormalVector(p)
-    ma = vector.magnitude(p[:3])
-    return vector.dotproduct(x, n) - p[3] / ma
+class Mirror:
+    '''
+    Utilities for getting a mirror image of a scaffold.
+    '''
+    def __init__(self, mirrorPlane):
+        '''
+        :param mirrorPlane: Plane ax+by+cd=d defined as a list p=[a,b,c,d].
+        '''
+        self._plane = mirrorPlane
+        self._planeUnitNormalVector = self.getPlaneNormalVector()
+        self._magNormal = vector.magnitude(mirrorPlane[:3])
+        self._planeDParam = mirrorPlane[3]
 
-def getPlaneNormalVector(p):
-    """
-    Get plane normal vector
-    :param p:  Plane ax+by+cd=d defined as a list p=[a,b,c,d].
-    :return: Unit normal vector.
-    """
-    return vector.normalise([p[0], p[1], p[2]])
+    def getPointDistanceFromPlane(self, x):
+        """
+        Get distance from point x to plane p. Returns negative distance, if point x is on the negative side of the plane.
+        :param x: Point coordinates.
+        :return: Distance.
+        """
+        n = self._planeUnitNormalVector
+        ma = self._magNormal
+        d = self._planeDParam
+        return vector.dotproduct(x, n) - d / ma
 
-def pointProjectionToPlane(x, p):
-    """
-    Find the projection of point x onto plane p
-    :param x:  Point coordinates.
-    :param p: Plane ax+by+cd=d defined as a list p=[a,b,c,d].
-    :return: x0, the point projection onto the plane
-    """
-    D = getPointDistanceFromPlane(x, p)
-    n = getPlaneNormalVector(p)
-    return [x[c] - D*n[c] for c in range(3)]
+    def getPlaneNormalVector(self):
+        """
+        Get plane normal vector
+        :return: Unit normal vector.
+        """
+        p = self._plane
+        return vector.normalise([p[0], p[1], p[2]])
 
-def mirrorImageOfPoint(x, p):
-    """
-    Find the mirror image of point x on plane p
-    :param x:  Point coordinates.
-    :param p: Plane ax+by+cd=d defined as a list p=[a,b,c,d].
-    :return: xm, mirror image of point x.
-    """
-    D = getPointDistanceFromPlane(x, p)
-    n = getPlaneNormalVector(p)
-    return [x[c] - 2*D*n[c] for c in range(3)]
+    def pointProjectionToPlane(self, x):
+        """
+        Find the projection of point x onto plane p
+        :param x:  Point coordinates.
+        :return: x0, the point projection onto the plane
+        """
+        D = self.getPointDistanceFromPlane(x)
+        n = self._planeUnitNormalVector
+        return [x[c] - D*n[c] for c in range(3)]
 
-def mirrorVector(v, p, keepSign = True):
-    """
-    Find the mirror image of vector v on plane p
-    :param v: Vector.
-    :param p: Plane.
-    :param keepSign: If True, negative of the vector is returned.
-    :return: image vector.
-    """
-    s = 1 if keepSign else -1
-    n = getPlaneNormalVector(p)
-    return [s*v[c] - s*2 * vector.dotproduct(v, n) * n[c] for c in range(3)]
+    def mirrorImageOfPoint(self, x):
+        """
+        Find the mirror image of point x on plane p
+        :param x:  Point coordinates.
+        :return: xm, mirror image of point x.
+        """
+        D = self.getPointDistanceFromPlane(x)
+        n = self._planeUnitNormalVector
+        return [x[c] - 2*D*n[c] for c in range(3)]
+
+    def mirrorVector(self,v):
+        """
+        Find the mirror image of vector v on plane p
+        :param v: Vector.
+        :return: image vector.
+        """
+        n = self._planeUnitNormalVector
+        return [(v[c] - 2 * vector.dotproduct(v, n) * n[c]) for c in range(3)]
+
+    def reverseMirrorVector(self,v):
+        """
+        Find the reverse mirror image of vector v on plane p
+        :param v: Vector.
+        :return: image vector.
+        """
+        n = self._planeUnitNormalVector
+        return [-v[c] + 2 * vector.dotproduct(v, n) * n[c] for c in range(3)]
