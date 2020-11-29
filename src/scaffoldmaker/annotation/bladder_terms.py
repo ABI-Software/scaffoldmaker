fd7ae7785cc00eed192a06da5034385b07a13cc3
Zohreh Ekhlasi <zekh167@aucklanduni.ac.nz>
2020-11-30 10:09:09 +1300

    Added new terms for bladder.

--- src/scaffoldmaker/annotation/bladder_terms.py
+++ src/scaffoldmaker/annotation/bladder_terms.py
@@ -4,14 +4,16 @@ Common resource for bladder annotation terms.
 
 # convention: preferred name, preferred id, followed by any other ids and alternative names
 bladder_terms = [
-    ("urinary bladder", "FMA:15900", "UBERON:0001255"),
-    ("neck of urinary bladder", "FMA:15912", "UBERON:0001258"),
+    ("urinary bladder", "UBERON:0001255", "FMA:15900"),
+    ("neck of urinary bladder", "UBERON:0001258", "FMA:15912"),
     ("Dome of the Bladder", "ILX:0738433"),
+    ("serosa of urinary bladder", "UBERON:0001260"),
+    ("bladder lumen", "UBERON:0009958"),
     ("serosa of body of urinary bladder", "ILX:0739276"),
     ("lumen of body of urinary bladder", "ILX:0739252"),
     ("serosa of neck of urinary bladder", "ILX:0739277"),
     ("lumen of neck of urinary bladder", "ILX:0739256"),
-    ("urethra", "ILX:0733022"),
+    ("urethra", "ILX:0733022", "UBERON:0000057"),
     ("lumen of urethra", "ILX:0736762"),
     ("serosa of urethra", "ILX:0739282")
 ]
