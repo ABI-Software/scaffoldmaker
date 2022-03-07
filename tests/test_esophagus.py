58e882c5bd031f280b15038640addd889dc334a0
Mabelle Lin <mlin865@aucklanduni.ac.nz>
2022-03-08 12:54:06 +1300

    Add meshtype, terms and unit test for esophagus

---
+++ tests/test_esophagus.py
@@ -0,0 +1,145 @@
+import copy
+import unittest
+
+from opencmiss.utils.zinc.finiteelement import evaluateFieldNodesetRange
+from opencmiss.utils.zinc.general import ChangeManager
+from opencmiss.zinc.context import Context
+from opencmiss.zinc.element import Element
+from opencmiss.zinc.field import Field
+from opencmiss.zinc.result import RESULT_OK
+from scaffoldmaker.annotation.annotationgroup import getAnnotationGroupForTerm
+from scaffoldmaker.annotation.esophagus_terms import get_esophagus_term
+from scaffoldmaker.meshtypes.meshtype_3d_esophagus1 import MeshType_3d_esophagus1
+from scaffoldmaker.utils.meshrefinement import MeshRefinement
+from scaffoldmaker.utils.zinc_utils import createFaceMeshGroupExteriorOnFace
+
+from testutils import assertAlmostEqualList
+
+
+class EsophagusScaffoldTestCase(unittest.TestCase):
+
+    def test_esophagus1(self):
+        """
+        Test creation of esophagus scaffold.
+        """
+        scaffold = MeshType_3d_esophagus1
+        parameterSetNames = scaffold.getParameterSetNames()
+        self.assertEqual(parameterSetNames, ["Default", "Human 1"])
+        options = scaffold.getDefaultOptions("Human 1")
+        self.assertEqual(15, len(options))
+        self.assertEqual(8, options.get("Number of elements around"))
+        self.assertEqual(12, options.get("Number of elements along"))
+        self.assertEqual(4, options.get("Number of elements through wall"))
+        self.assertEqual(5.0, options.get("Wall thickness"))
+        self.assertEqual(0.35, options.get("Mucosa relative thickness"))
+        self.assertEqual(0.15, options.get("Submucosa relative thickness"))
+        self.assertEqual(0.25, options.get("Circular muscle layer relative thickness"))
+        self.assertEqual(0.25, options.get("Longitudinal muscle layer relative thickness"))
+
+        context = Context("Test")
+        region = context.getDefaultRegion()
+        self.assertTrue(region.isValid())
+        annotationGroups = scaffold.generateBaseMesh(region, options)
+        self.assertEqual(8, len(annotationGroups))
+
+        fieldmodule = region.getFieldmodule()
+        self.assertEqual(RESULT_OK, fieldmodule.defineAllFaces())
+        mesh3d = fieldmodule.findMeshByDimension(3)
+        self.assertEqual(384, mesh3d.getSize())
+        mesh2d = fieldmodule.findMeshByDimension(2)
+        self.assertEqual(1280, mesh2d.getSize())
+        mesh1d = fieldmodule.findMeshByDimension(1)
+        self.assertEqual(1416, mesh1d.getSize())
+        nodes = fieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        self.assertEqual(520, nodes.getSize())
+        datapoints = fieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_DATAPOINTS)
+        self.assertEqual(0, datapoints.getSize())
+
+        coordinates = fieldmodule.findFieldByName("coordinates").castFiniteElement()
+        self.assertTrue(coordinates.isValid())
+        minimums, maximums = evaluateFieldNodesetRange(coordinates, nodes)
+        assertAlmostEqualList(self, minimums, [-17.57777917463216, -9.777573632674187, -14.999995678650333], 1.0E-6)
+        assertAlmostEqualList(self, maximums, [40.16077850310996, 291.0400575669104, 14.999999571130854], 1.0E-6)
+
+        with ChangeManager(fieldmodule):
+            one = fieldmodule.createFieldConstant(1.0)
+            faceMeshGroup = createFaceMeshGroupExteriorOnFace(fieldmodule, Element.FACE_TYPE_XI3_1)
+            surfaceAreaField = fieldmodule.createFieldMeshIntegral(one, coordinates, faceMeshGroup)
+            surfaceAreaField.setNumbersOfPoints(4)
+            volumeField = fieldmodule.createFieldMeshIntegral(one, coordinates, mesh3d)
+            volumeField.setNumbersOfPoints(3)
+        fieldcache = fieldmodule.createFieldcache()
+        result, surfaceArea = surfaceAreaField.evaluateReal(fieldcache, 1)
+        self.assertEqual(result, RESULT_OK)
+        self.assertAlmostEqual(surfaceArea, 27931.063402890817, delta=1.0E-6)
+        result, volume = volumeField.evaluateReal(fieldcache, 1)
+        self.assertEqual(result, RESULT_OK)
+        self.assertAlmostEqual(volume, 116538.12918074048, delta=1.0E-6)
+
+        # check some annotationGroups:
+        expectedSizes3d = {
+            "abdominal part of esophagus": 64,
+            "cervical part of esophagus": 64,
+            "esophagus": 384,
+            "esophagus mucosa": 96,
+            "esophagus smooth muscle circular layer": 96,
+            "esophagus smooth muscle longitudinal layer": 96,
+            "submucosa of esophagus": 96,
+            "thoracic part of esophagus": 256
+            }
+        for name in expectedSizes3d:
+            group = getAnnotationGroupForTerm(annotationGroups, get_esophagus_term(name))
+            size = group.getMeshGroup(mesh3d).getSize()
+            self.assertEqual(expectedSizes3d[name], size, name)
+
+        # refine 4x4x4 and check result
+        # first remove any surface annotation groups as they are re-added by defineFaceAnnotations
+        removeAnnotationGroups = []
+        for annotationGroup in annotationGroups:
+            if (not annotationGroup.hasMeshGroup(mesh3d)) and \
+                    (annotationGroup.hasMeshGroup(mesh2d) or annotationGroup.hasMeshGroup(mesh1d)):
+                removeAnnotationGroups.append(annotationGroup)
+
+        for annotationGroup in removeAnnotationGroups:
+            annotationGroups.remove(annotationGroup)
+        self.assertEqual(8, len(annotationGroups))
+
+        refineRegion = region.createRegion()
+        refineFieldmodule = refineRegion.getFieldmodule()
+        options['Refine number of elements around'] = 4
+        options['Refine number of elements along'] = 4
+        options['Refine number of elements through wall'] = 4
+        meshrefinement = MeshRefinement(region, refineRegion, annotationGroups)
+        scaffold.refineMesh(meshrefinement, options)
+        annotationGroups = meshrefinement.getAnnotationGroups()
+
+        refineFieldmodule.defineAllFaces()
+        oldAnnotationGroups = copy.copy(annotationGroups)
+        for annotationGroup in annotationGroups:
+            annotationGroup.addSubelements()
+        scaffold.defineFaceAnnotations(refineRegion, options, annotationGroups)
+        for annotation in annotationGroups:
+            if annotation not in oldAnnotationGroups:
+                annotationGroup.addSubelements()
+        self.assertEqual(9, len(annotationGroups))
+#
+        mesh3d = refineFieldmodule.findMeshByDimension(3)
+        self.assertEqual(24576, mesh3d.getSize())
+        mesh2d = refineFieldmodule.findMeshByDimension(2)
+        self.assertEqual(75776, mesh2d.getSize())
+        mesh1d = refineFieldmodule.findMeshByDimension(1)
+        self.assertEqual(77856, mesh1d.getSize())
+        nodes = refineFieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        self.assertEqual(26656, nodes.getSize())
+        datapoints = refineFieldmodule.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_DATAPOINTS)
+        self.assertEqual(0, datapoints.getSize())
+
+        # check some refined annotationGroups:
+        for name in expectedSizes3d:
+            group = getAnnotationGroupForTerm(annotationGroups, get_esophagus_term(name))
+            size = group.getMeshGroup(mesh3d).getSize()
+            self.assertEqual(expectedSizes3d[name]*64, size, name)
+
+
+if __name__ == "__main__":
+    unittest.main()
