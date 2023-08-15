8d05ff8e1dea15ad0af78e9aeb4519885fe31fa5
Zohreh Ekhlasi <zekh167@aucklanduni.ac.nz>
2023-08-15 12:41:32 +1200

    Updated colon segment test.

--- tests/test_colonsegment.py
+++ tests/test_colonsegment.py
@@ -78,10 +78,10 @@ class ColonSegmentScaffoldTestCase(unittest.TestCase):
         fieldcache = fieldmodule.createFieldcache()
         result, surfaceArea = surfaceAreaField.evaluateReal(fieldcache, 1)
         self.assertEqual(result, RESULT_OK)
-        self.assertAlmostEqual(surfaceArea, 21035.8818469729, delta=1.0E-6)
+        self.assertAlmostEqual(surfaceArea, 21060.83216104569, delta=1.0E-6)
         result, volume = volumeField.evaluateReal(fieldcache, 1)
         self.assertEqual(result, RESULT_OK)
-        self.assertAlmostEqual(volume, 39781.704358310606, delta=1.0E-6)
+        self.assertAlmostEqual(volume, 39250.883633327816, delta=1.0E-6)
 
     def test_mousecolonsegment1(self):
         """
