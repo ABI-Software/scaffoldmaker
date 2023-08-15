ea67b232fc8e48ef3ff6bef3b9cd355aa6f654c5
Zohreh Ekhlasi <zekh167@aucklanduni.ac.nz>
2023-08-15 15:45:36 +1200

    Updated colonsegment test with main branch.

--- tests/test_colonsegment.py
+++ tests/test_colonsegment.py
@@ -78,10 +78,10 @@ class ColonSegmentScaffoldTestCase(unittest.TestCase):
         fieldcache = fieldmodule.createFieldcache()
         result, surfaceArea = surfaceAreaField.evaluateReal(fieldcache, 1)
         self.assertEqual(result, RESULT_OK)
-        self.assertAlmostEqual(surfaceArea, 21060.83216104569, delta=1.0E-6)
+        self.assertAlmostEqual(surfaceArea, 21035.8818469729, delta=1.0E-6)
         result, volume = volumeField.evaluateReal(fieldcache, 1)
         self.assertEqual(result, RESULT_OK)
-        self.assertAlmostEqual(volume, 39250.883633327816, delta=1.0E-6)
+        self.assertAlmostEqual(volume, 39781.704358310606, delta=1.0E-6)
 
     def test_mousecolonsegment1(self):
         """
@@ -114,10 +114,10 @@ class ColonSegmentScaffoldTestCase(unittest.TestCase):
         fieldcache = fieldmodule.createFieldcache()
         result, surfaceArea = surfaceAreaField.evaluateReal(fieldcache, 1)
         self.assertEqual(result, RESULT_OK)
-        self.assertAlmostEqual(surfaceArea, 460.6135154899307, delta=1.0E-6)
+        self.assertAlmostEqual(surfaceArea, 468.17062489996886, delta=1.0E-6)
         result, flatSurfaceArea = flatSurfaceAreaField.evaluateReal(fieldcache, 1)
         self.assertEqual(result, RESULT_OK)
-        self.assertAlmostEqual(flatSurfaceArea, 468.1706248998369, delta=1.0E-3)
+        self.assertAlmostEqual(flatSurfaceArea, surfaceArea, delta=1.0E-3)
 
 
 if __name__ == "__main__":
