51369abb7323f36741a0a96df7bd4f712e4a20fc
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-07-23 20:37:18 +1200

    Revert change to GI tract answer, seems this is a machine tolerance error problem.

--- tests/test_gastrointestinaltract1.py
+++ tests/test_gastrointestinaltract1.py
@@ -113,7 +113,7 @@ class GastrointestinalTractScaffoldTestCase(unittest.TestCase):
         self.assertAlmostEqual(surfaceArea, 280350.6462177311, delta=1.0E-6)
         result, volume = volumeField.evaluateReal(fieldcache, 1)
         self.assertEqual(result, RESULT_OK)
-        self.assertAlmostEqual(volume, 598505.8880710538, delta=1.0E-6)
+        self.assertAlmostEqual(volume, 598505.4701647585, delta=1.0E-6)
 
         # check some annotationGroups:
         expectedSizes3d = {
