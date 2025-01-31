0c7e9587bf842a73ae311aedb002a96a95677614
v-chopovda <valerie.chopovda@auckland.ac.nz>
2025-01-31 14:12:59 +1300

    updated tests

--- tests/test_vagus.py
+++ tests/test_vagus.py
@@ -39,23 +39,39 @@ class VagusScaffoldTestCase(unittest.TestCase):
         assert result == RESULT_OK
 
         vagus_data = VagusInputData(data_region)
+        self.assertEqual(vagus_data.get_side_label(), 'right')
 
         marker_data = vagus_data.get_level_markers()
         self.assertEqual(len(marker_data), 2)
+        assert 'right level of superior border of the clavicle on the vagus nerve' in marker_data
+
         orientation_data = vagus_data.get_orientation_data()
         self.assertEqual(len(orientation_data), 1)
         self.assertEqual(len(orientation_data['orientation anterior']), 3)
 
         trunk_group_name = vagus_data.get_trunk_group_name()
-        self.assertEqual(trunk_group_name, "left vagus X nerve trunk")
-        branch_data = vagus_data.get_branch_data()
-        self.assertEqual(len(branch_data), 3)
+        self.assertEqual(trunk_group_name, "right vagus X nerve trunk")
+        trunk_coordinates = vagus_data.get_trunk_coordinates()
+        self.assertEqual(len(trunk_coordinates), 32)
         annotation_term_map = vagus_data.get_annotation_term_map()
         assert trunk_group_name in annotation_term_map
+        self.assertEqual(annotation_term_map[trunk_group_name], 'http://purl.obolibrary.org/obo/UBERON_0035020')
+
+        branch_data = vagus_data.get_branch_data()
+        self.assertEqual(len(branch_data), 3)
+        assert 'right B branch' in branch_data
+        self.assertEqual(len(branch_data['right A branch']), 5)
+        assert 'right A branch of branch B' in branch_data
+        self.assertEqual(len(branch_data['right A branch of branch B']), 2)
+
+        branch_parents = vagus_data.get_branch_parent_map()
+        self.assertEqual(branch_parents['right B branch'], trunk_group_name)
+        self.assertEqual(branch_parents['right A branch of branch B'], 'right B branch')
 
         branch_common_groups = vagus_data.get_branch_common_group_map()
-        print(branch_common_groups)
-
+        self.assertEqual(len(branch_common_groups), 1)
+        assert 'right branches' in branch_common_groups
+        assert 'right B branch' in branch_common_groups['right branches']
 
     def test_vagus_box(self):
         """
