1c597e14d2d1c4887ab9b3fbc561c919e1257b14
0Huitzil <frcastaneda@protonmail.com>
2025-06-18 14:27:00 +1200

    Added muscle test file Added annotation test (UBERON/ILX) for muscle

---
+++ tests/test_muscle.py
@@ -0,0 +1,28 @@
+import unittest
+
+from cmlibs.utils.zinc.finiteelement import evaluateFieldNodesetRange
+from cmlibs.utils.zinc.general import ChangeManager
+from cmlibs.zinc.context import Context
+from cmlibs.zinc.element import Element
+from cmlibs.zinc.field import Field
+from cmlibs.zinc.result import RESULT_OK
+from scaffoldmaker.annotation.annotationgroup import getAnnotationGroupForTerm
+from scaffoldmaker.annotation.muscle_terms import get_muscle_term, muscle_terms
+from scaffoldmaker.meshtypes.meshtype_3d_musclefusiform1 import MeshType_3d_musclefusiform1
+from scaffoldmaker.utils.zinc_utils import createFaceMeshGroupExteriorOnFace
+
+from testutils import assertAlmostEqualList
+
+
+class MuscleScaffoldTestCase(unittest.TestCase):
+
+    def test_muscle_annotations(self):
+        """
+        Test that all muscle terms are UBERON or ILX.
+        """
+        for term in muscle_terms:
+            upper_id = term[1].upper()
+            self.assertTrue(("UBERON" in upper_id) or ("ILX" in upper_id), "Invalid heart term" + str(term))
+
+if __name__ == "__main__":
+    unittest.main()
